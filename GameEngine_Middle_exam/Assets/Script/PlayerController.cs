aec4d2025ac5568fa834fe488864470ed87fc38f
dotcom4143 <eostmfhektzja4143@gmail.com>
2026-04-22 06:24:14 +0900

    마을 구성 완성 코딩은 ㅎㅎ

---
+++ GameEngine_Middle_exam/Assets/Script/PlayerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+
+    private Rigidbody2D rb;
+    private float moveInput;
+
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundRadius,
+            groundLayer
+        );
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+}
--- GameEngine_Middle_exam/Assets/Script/싹다 갈아치우고 진짜 스크립트/MouseFollower.cs
+++ GameEngine_Middle_exam/Assets/Script/싹다 갈아치우고 진짜 스크립트/MouseFollower.cs
@@ -1,53 +1,17 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class MouseFollower : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 7f;
-
-    private Rigidbody2D rb;
-    private float moveInput;
-
-    public Transform groundCheck;
-    public float groundRadius = 0.2f;
-    public LayerMask groundLayer;
-
-    private bool isGrounded;
-
-    public Transform currentHook;
-    public bool isHooked;
-    public void SetHook(Transform hook)
-    {
-        currentHook = hook; isHooked = true;
-    }
-    public void ReleaseHook()
-    {
-        currentHook = null; isHooked = false;
-    }
-    
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    public float zDistance = 10f;
 
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = zDistance;
 
-        isGrounded = Physics2D.OverlapCircle(
-            groundCheck.position,
-            groundRadius,
-            groundLayer
-        );
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        worldPos.z = 0f;
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        transform.position = worldPos;
     }
 }
